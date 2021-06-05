c943764d74f439eb33b6692ea41a098afa9a20cf
Daria Kozina <kozina.flowers@yandex.ru>
2021-06-05 10:46:40 +0300

    all files added to commit

---
+++ show_info.js
@@ -0,0 +1,40 @@
+//last edited 09.03.21
+// получить кнопку 1, 2
+// при наведении мыши на 1 показать кнопку 2
+//  при убирании мыши с 2 удалить класс
+// при клике на 2 перейти по ссылке
+function showInfo(sel1, sel2, link) {
+    const btn1 =  document.querySelector(sel1);
+    const btn2 = document.querySelector(sel2);
+    btn1.addEventListener('mouseover', () => {
+        btn2.classList.add('show');
+    });
+
+    btn2.addEventListener('mouseout', () => {
+        btn2.classList.remove('show');
+    });
+    btn2.addEventListener('click', () => {
+        window.location.href = link;
+    });
+}
+
+
+
+showInfo('.insane', '.insane_info', 'https://dariablomma.github.io/insane-diploma/src/layout/');
+
+showInfo('.glo3d', '.glo3d_info', 'https://dariablomma.github.io/3dglo/');
+
+showInfo('.glo_project1', '.glo_project1_info', 'https://dariablomma.github.io/GloAcademy.18.0/Project%201/Obligatory%20tasks/');
+
+showInfo('.scandi', '.scandi_info', 'https://dariablomma.github.io/HTMLAcademy.-Marathon-3.0/');
+
+showInfo('.logitech', '.logitech_info', '/logitech');
+
+showInfo('.your_house', '.house_info', '/your_house/main_page.html');
+
+showInfo('.summer_trip', '.trip_info', 'https://mega.nz/file/TzoQjThR#O4b4OtFfjLxe4YL_C9wuzl1PCDiSWUdoklvYwZUpQN4');
+
+showInfo('.odysseum', '.odysseum_info', 'https://www.figma.com/file/5x09vTmuMaxHx6PNY3Rc69/Page3.-Design.Odysseum');
+
+showInfo('.photoshop', '.photoshop_info', 'https://www.dropbox.com/s/ezb68rhsbiusrgi/min%20swedish.psd?dl=0');
+
---
+++ slideshow.js
@@ -0,0 +1,74 @@
+const prevBtn = document.getElementById('btn-prev'),
+    nextBtn = document.getElementById('btn-next'),
+    slides = document.querySelectorAll('.slide'),
+    dotsWrapper = document.querySelector('.dots-wrapper');
+
+let index = 0;
+const renderDots = () => {
+    const dot = document.createElement('span');
+    dot.className = 'dot';
+    dotsWrapper.append(dot);
+};
+
+for (let i = 0; i < slides.length; i++) {
+    renderDots();
+}
+
+const dots = document.querySelectorAll('.dot');
+
+function activeSlide() {
+    for (const slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[index].classList.add('active');
+}
+
+function activeDot() {
+    for (const dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[index].classList.add('active');
+}
+
+function prepareCurrentSlide() {
+    activeSlide(index);
+    activeDot(index);
+}
+function nextSlide() {
+    if (index === slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    } else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+function prevSlide() {
+    if (index === 0) {
+        index = slides.length - 1;
+        prepareCurrentSlide(index);
+    } else {
+        index--;
+        prepareCurrentSlide(index);
+    }
+}
+
+
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    });
+});
+
+const autoChange = setInterval(nextSlide, 3000);
+
+for (const slide of slides) {
+    slide.addEventListener('mouseover', () => {
+        clearInterval(autoChange);
+    });
+}
